b115c88a0f1e07c1938618e6c288974cb26d5607
Preston Osoro <prestonosoro56@gmail.com>
2025-05-01 01:18:00 +0300

    chore(hooks): import initialWorkstations from mocks

--- hooks/use-workstations.ts
+++ hooks/use-workstations.ts
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { initialWorkstations } from "@/app/admin/mocks/data"
+import { initialWorkstations } from "@/mocks"
 import { useToast } from "@/hooks/use-sonner"
 
 export const useWorkstations = () => {
