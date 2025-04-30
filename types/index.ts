1387cfa1e0286e218b4ce19d847699e40a5fe3a2
Preston Osoro <prestonosoro56@gmail.com>
2025-05-01 01:01:25 +0300

    chore(types): centralize type exports in index.ts

---
+++ types/index.ts
@@ -0,0 +1,11 @@
+export * from "@/types/book";
+export * from "@/types/booking";
+export * from "@/types/cluster";
+export * from "@/types/floor";
+export * from "@/types/hot-desk";
+export * from "@/types/stats";
+export * from "@/types/user";
+export * from "@/types/workstation";
+
+
+
