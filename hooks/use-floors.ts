d77edf141d6f661483574ca1c4807aef948880cd
Preston Osoro <prestonosoro56@gmail.com>
2025-05-01 01:19:28 +0300

    chore(hooks): import initialFloors from mocks

--- hooks/use-floors.ts
+++ hooks/use-floors.ts
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { initialFloors } from "@/app/admin/mocks/data";
+import { initialFloors } from "@/mocks";
 import { useToast } from "@/hooks/use-sonner";
 
 export const useFloors = () => {
