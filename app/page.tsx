7ccef13bf68e8860a4f0ed826bb073d66156729a
Preston Osoro <prestonosoro56@gmail.com>
2025-05-21 20:34:59 +0300

    feat: adjust header tag to h2

--- app/page.tsx
+++ app/page.tsx
@@ -12,9 +12,9 @@ export default function Home() {
           <div className="container px-4 md:px-6 lg:px-12">
             <div className="flex flex-col items-center justify-center space-y-4 text-center">
               <div className="space-y-2">
-                <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
+                <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
                   Spotter Labs Workstation Booking
-                </h1>
+                </h2>
                 <p className="mx-auto max-w-[700px] text-muted-foreground md:text-xl">
                   Book your workstation easily and efficiently. Find available
                   hot desks based on your cluster.
